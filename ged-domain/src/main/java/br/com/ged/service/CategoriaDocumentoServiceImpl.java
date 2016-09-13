61168a7f5875415462e731464c841500b97b0b8b
Pedro Augusto <pedroaugusto.gti@gmail.com>
2016-09-13 10:48:24 -0300

    Customização do layout com a adição das logos no cabeçalho da aplicação. Correção do bug para remover categorias e sub categorias com documentos vinculados.

--- ged-domain/src/main/java/br/com/ged/service/CategoriaDocumentoServiceImpl.java
+++ ged-domain/src/main/java/br/com/ged/service/CategoriaDocumentoServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.ged.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -8,7 +9,6 @@ import javax.persistence.Query;
 
 import br.com.ged.domain.Mensagem;
 import br.com.ged.dto.FiltroCategoriaDocumentoDTO;
-import br.com.ged.dto.FiltroDocumentoDTO;
 import br.com.ged.entidades.Categoria;
 import br.com.ged.entidades.Documento;
 import br.com.ged.excecao.NegocioException;
@@ -37,28 +37,52 @@ public class CategoriaDocumentoServiceImpl extends ConsultasDaoJpa<Categoria> im
 		
 		try{
 			
-			FiltroDocumentoDTO filtroDocumentoDTO = new FiltroDocumentoDTO();
-			
-			filtroDocumentoDTO.setIdCategoria(categoriaSelecionada.getId());
-			
-			List<Documento> documentosCategoria = reposirotyDocumento.filtrarPesquisa(filtroDocumentoDTO, Documento.class);
-			
-			for (Documento doc : documentosCategoria){
+			for (Long idSubCategoria : extraiIdsSubCategoriasSelecionadas(categoriaSelecionada.getCategoriaFilha())){
 				
-				genericPersistenceDocumento.excluir(doc);
+				excluiCategoria(idSubCategoria);
 			}
 			
-			Query query = em.createNativeQuery("DELETE FROM rl_categoria_grupousuario WHERE id_categoria = :id");
-			Query queryCategoria = em.createNativeQuery("DELETE FROM tb_categoria WHERE id_categoria = :id");
-			
-			query.setParameter("id", categoriaSelecionada.getId());
-			queryCategoria.setParameter("id", categoriaSelecionada.getId());
-			query.executeUpdate();
-			queryCategoria.executeUpdate();
+			excluiCategoria(categoriaSelecionada.getId());
 			
 		}catch(Exception e){
 			e.printStackTrace();
 			throw new NegocioException(Mensagem.ERRO_DEFAULT);
 		}
+	}
+
+	private void excluiCategoria(Long idCategoria) {
+		
+		
+		Query queryDocumento = em.createNativeQuery("DELETE FROM tb_documento WHERE id_categoria = :id");
+		Query query = em.createNativeQuery("DELETE FROM rl_categoria_grupousuario WHERE id_categoria = :id");
+		Query queryCategoria = em.createNativeQuery("DELETE FROM tb_categoria WHERE id_categoria = :id");
+		
+		queryDocumento.setParameter("id", idCategoria);
+		query.setParameter("id", idCategoria);
+		queryCategoria.setParameter("id", idCategoria);
+		
+		queryDocumento.executeUpdate();
+		query.executeUpdate();
+		queryCategoria.executeUpdate();
+	}
+	
+	private List<Long> extraiIdsSubCategoriasSelecionadas(List<Categoria> categoriaFilha) {
+		
+		if (categoriaFilha == null){
+			return null;
+		}
+		
+		List<Long> ids = new ArrayList<>();
+		
+		for (Categoria cats : categoriaFilha){
+			
+			ids.add(cats.getId());
+			
+			if (cats.getCategoriaFilha() != null && !cats.getCategoriaFilha().isEmpty()){
+				ids.addAll(extraiIdsSubCategoriasSelecionadas(cats.getCategoriaFilha()));
+			}
+		}
+		
+		return ids;
 	}
 }
